8a8b5684ddfbfb849b5de3c661c00ec35bc2505d
DanielNeves <dbenditoneves@gmail.com>
2020-08-20 10:36:46 -0300

    Register pronto

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,16 @@
+class RegisterController < ApplicationController
+  def sign_up
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password_digest)
+    end
+end
